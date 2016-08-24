44c426e7160604f2c01a1d89c6a916792737d58f
Gang Chen <gangchen@us.ibm.com>
2016-08-24 11:05:25 -0500

    Update to the Model class
    Signed-off-by: Gang Chen <gangchen@us.ibm.com>

--- socialreview/common/models/review.js
+++ socialreview/common/models/review.js
@@ -4,20 +4,20 @@ module.exports = function(Review) {
   Review.observe('access', function(ctx, next) {
 
     console.log('> Review access triggered');
-    var context = loopback.getCurrentContext();
-    var itemId = context.active.http.req.query.itemId;
+    //var context = loopback.getCurrentContext();
+    //var itemId = context.active.http.req.query.itemId;
 
-    var model = ctx.instance;
-    var reviewService = Review.app.dataSources.Review;
+    //var model = ctx.instance;
+    var reviewService = app.dataSources.Review;
 
-    reviewService.find("1024", function(err, response, context) {
+    reviewService.find({"itemId": "1024"}, function(err, response, context) {
       if (err) throw err; //error making request
       if (response.error) {
         next('> response error: ' + response.error.stack);
       }
       model.review = response;
       console.log('> data fetched successfully from remote server');
-      //verify via `curl localhost:3000/api/Magazines`
+      //verify via `curl localhost:3000/api/review`
       next();
     });
   });
