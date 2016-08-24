7d1db07d7e51e45da71c783581fd4a4037f8397c
Gang Chen <gangchen@us.ibm.com>
2016-08-24 10:44:32 -0500

    initial Loopback REST connector code checked in
    Signed-off-by: Gang Chen <gangchen@us.ibm.com>

---
+++ socialreview/common/models/review.js
@@ -0,0 +1,25 @@
+module.exports = function(Review) {
+
+ // Handle the reivew GET operation
+  Review.observe('access', function(ctx, next) {
+
+    console.log('> Review access triggered');
+    var context = loopback.getCurrentContext();
+    var itemId = context.active.http.req.query.itemId;
+
+    var model = ctx.instance;
+    var reviewService = Review.app.dataSources.Review;
+
+    reviewService.find("1024", function(err, response, context) {
+      if (err) throw err; //error making request
+      if (response.error) {
+        next('> response error: ' + response.error.stack);
+      }
+      model.review = response;
+      console.log('> data fetched successfully from remote server');
+      //verify via `curl localhost:3000/api/Magazines`
+      next();
+    });
+  });
+
+};
