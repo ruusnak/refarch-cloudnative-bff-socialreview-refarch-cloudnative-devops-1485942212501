53993c91d1abe5a576dcd8cbfa0f5d15568ac92a
sreenivas@us.ibm.com <sreenivas@us.ibm.com>
2016-09-21 16:27:33 -0500

    set zuul url with microservice appname and context path
    Signed-off-by: sreenivas@us.ibm.com <sreenivas@us.ibm.com>

--- socialreview/set-zuul-proxy-url.sh
+++ socialreview/set-zuul-proxy-url.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
- 
-sed -i -e "s|\(\"baseURL\".*http:\/\/\)\(.*\)\(\/micro.*\)|\1$1\3|" server/datasources.json
+
+MICROSERVICE_NAME=socialreview-microservice
+CONTEXT_PATH=micro
+
+while [[ $# -gt 1 ]]
+do
+arg="$1"
+case $arg in
+    -m|--msname)
+    MICROSERVICE_NAME="$2"
+    shift;;
+    -c|--context)
+    CONTEXT_PATH="$2"
+    shift;;
+    -z|--zuul)
+    ZUUL_FQDN=$(echo ${2}|sed -e "s/http.*\/\///" -e "s/\/$//")
+    shift;;
+    *)
+esac
+shift
+done
+
+ZUULPROXY="${ZUUL_FQDN}/${MICROSERVICE_NAME}/${CONTEXT_PATH}"
+echo "Zuul Proxy URL: $ZUULPROXY"
+sed -e "s|\(\"baseURL\".*http:\/\/\)\(.*\)\(\/review.*\)|\1$ZUULPROXY\3|" server/datasources.json
+sed -i -e "s|\(\"url\".*http:\/\/\)\(.*\)\(\/review.*\)|\1$ZUULPROXY\3|g" server/datasources.json
 cat server/datasources.json|grep baseURL
